b975da1567f1e5c997d4652940b42bd684adc034
matheuspenachioni <matheus.penachioni2001@gmail.com>
2023-03-18 18:02:56 -0300

    project created

---
+++ ft-customer/src/test/java/br/com/matheus/ftcustomer/FtCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matheus.ftcustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
